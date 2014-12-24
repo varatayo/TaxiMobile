1b5a2fbbfa00a2a947fba336340ffa988f0f01cc
Gian Carlo Zamboni <gzamboni@colabra.cl>
2014-12-24 17:26:07 +0000

    Changes made by Gian Carlo Zamboni (gzamboni@colabra.cl).

--- Cliente Flash 2.0/scripts/app/propuesta.js
+++ Cliente Flash 2.0/scripts/app/propuesta.js
@@ -5,13 +5,21 @@
     aceptarCarreraViewModel = kendo.data.ObservableObject.extend({
 
         acepta: function () {
-            var fechaHora = app.formattedDate;
-            app.showAlert(fechaHora);
+            var date = new Date();
+            var fechaHora = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDay() + "T" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+            
             $.post("http://rtflash.azurewebsites.net/movil/solicitud",
-                { IdUsuario: window.localStorage["username"], FechaHora: fechaHora, Latitud: -33.437818, Longitud: -70.634398, Comentario: "" },
+                { IdUsuario: 1, FechaHora: fechaHora, Latitud: -33.437818, Longitud: -70.634398, Comentario: "" },
                 function (res) {
-                    app.showAlert(res);
-                    //app.showAlert("El usuario o password ingresados no son correctas.");
+                    if (res.IdSolicitud != 0)
+                    {
+                        app.showAlert("La solicitud se ha creado correctamente.");
+                        app.mobileApp.navigate('views/mapView.html', 'slide');
+                    }
+                    else
+                    {
+                        app.showError(res.IdSolicitud);
+                    }
                 },
                 "json");
 
